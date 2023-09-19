4ed71db0ab2d29e443abc2d202156dfe40816dcb
diegoferesin <diegoferesin@gmail.com>
2023-09-19 17:16:19 -0300

    Adding the Get and GetAll endpoints and a postman collection

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
+router.get('/ping', (req, res) => {
+    res.send('pong');
 });
+
+router.use('/users', require('./users'));
 
 module.exports = router;
