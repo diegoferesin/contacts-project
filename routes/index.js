4d8c5859bbf51b91ab13e90d825ddd9c1ee372db
diegoferesin <diegoferesin@gmail.com>
2023-09-21 08:40:06 -0300

    Adding swagger

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
-
+router.use('/api-docs', require('./swagger'));
 router.get('/', (req, res) => {
+    //#swagger.tag=['Hello World]
     res.send('Hello World!');
 });
 router.get('/ping', (req, res) => {
