630940f3ee4afc685f73262554cc8f7b58a6e694
diegoferesin <diegoferesin@gmail.com>
2023-10-03 00:36:13 -0300

    Adding validation

--- controllers/users.js
+++ controllers/users.js
@@ -3,21 +3,35 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger.tags = ['Users']
-    const result = await mongodb.getDatase().db().collection('contacts').find();
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
-    });
+    const result = await mongodb.getDatase().db().collection('contacts').find().toArray(
+        (err, lists) => {
+            if (err) {
+                res.status(500).json({ message: err });
+            } else {
+                res.setHeader('Content-Type', 'application/json');
+                res.status(200).json(lists);
+            }
+        }
+    );
 };
 
 const getSingle = async (req, res) => {
     //#swagger.tags = ['Users']
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json("Must use a valid contact id to find a contact.");
+    }
+
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatase().db().collection('contacts').find({_id: userId});
-    result.toArray().then((users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
-    });
+    const result = await mongodb.getDatase().db().collection('contacts').find({ _id: userId }).toArray(
+        (err, result) => {
+            if (err) {
+                res.status(500).json({ message: err });
+            } else {
+                res.setHeader('Content-Type', 'application/json');
+                res.status(200).json(result[0]);
+            }
+        }
+    );
 };
 
 const createUser = async (req, res) => {
@@ -31,14 +45,18 @@ const createUser = async (req, res) => {
     };
     const response = await mongodb.getDatase().db().collection('contacts').insertOne(user);
     if (response.acknowledged) {
-        res.status(204).send();
-    }else {
+        res.status(201).send();
+    } else {
         res.status(500).json(response.error || "Some error occurred while updating the user");
     }
 };
 
 const updateUser = async (req, res) => {
     //#swagger.tags = ['Users']
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json("Must use a valid contact id to update a contact.");
+    }
+
     const userId = new ObjectId(req.params.id);
     const user = {
         firstName: req.body.firstName,
@@ -47,21 +65,26 @@ const updateUser = async (req, res) => {
         favoriteColor: req.body.favoriteColor,
         birthdate: req.body.birthdate
     };
-    const response = await mongodb.getDatase().db().collection('contacts').replaceOne({_id: userId}, user);
+    const response = await mongodb.getDatase().db().collection('contacts').replaceOne({ _id: userId }, user);
     if (response.modifiedCount > 0) {
         res.status(204).send();
-    }else {
+    } else {
         res.status(500).json(response.error || "Some error occurred while updating the user");
     }
 };
 
 const deleteUser = async (req, res) => {
     //#swagger.tags = ['Users']
+
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json("Must use a valid contact id to delete a contact.");
+    }
+
     const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatase().db().collection('contacts').deleteOne({_id: userId});
+    const response = await mongodb.getDatase().db().collection('contacts').deleteOne({ _id: userId });
     if (response.deletedCount > 0) {
         res.status(204).send();
-    }else {
+    } else {
         res.status(500).json(response.error || "Some error occurred while deleting the user");
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/users');
+const validation = require('../middleware/validate');
 
 router.get('/', userController.getAll);
 
 router.get('/:id', userController.getSingle);
 
-router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
+router.post('/', validation.saveContact, userController.createUser);
+router.put('/:id', validation.saveContact, userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
